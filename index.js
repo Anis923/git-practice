8902359d8d7d4d27afed4e2e85b02acfa6dae01c
Anish923 <anishraja2112@gmail.com>
2026-01-11 21:52:51 +0530

    this is commit 1

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-console.log('Welcome to git');
-for(var i=0;i<5;i++){
-    console.log("raja");
-}
-=======
 console.log('Welcome to playlist');
 for(var i =0;i<5;i++){
-    console.log("anish");
+    console.log("ani");
 }
->>>>>>> main
